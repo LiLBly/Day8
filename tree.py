09c07ade64a42a1b4dc1556bc3d67fc550ea0c39
LiLBly <141209474+LiLBly@users.noreply.github.com>
2025-07-05 01:12:11 +0800

    first commit

---
+++ tree.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self,elem=-1,lchild=None,rchild=None):
+        self.elem=elem
+        self.lchild=lchild
+        self.rchild=rchild
+    
+class BinaryTree:
+    def __init__(self):
+        self.root=None
+        self.queue=[]
+    def level_build_tree(self,node:Node):
+        pass
